6f96a81f08d4424c15afa7be7bdc05cf9eaf97e6
nhnajibullah <nurhasan95@gmail.com>
2019-02-18 22:04:08 +0700

    change default route to show contact form page

--- src/routes/contactme.php
+++ src/routes/contactme.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['namespace' => 'Nadaft\ContactMe\Http\Controllers' , 'middleware' => 'web'], function () {
-    Route::get('contact', 'ContactMeController@showForm')->name('contactme.form');
+    Route::get('contactme', 'ContactMeController@showForm')->name('contactme.form');
     Route::post('contactme-submit', 'ContactMeController@send')->name('contactme.submit');
 });
